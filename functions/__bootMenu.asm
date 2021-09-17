b788cb5ab1d8c9f395c25932ef4a0f402a8cde77
kiwami <alienozioa@gmail.com>
2021-09-17 02:52:40 +0200

    Totan benim menü neden sadece 5 yaziyor\?

--- functions/__bootMenu.asm
+++ functions/__bootMenu.asm
@@ -3,13 +3,16 @@
 ; Author: Oguz
 ; see od -t x1 -A n xx.bin
 
+;#############################################################################################
+; 
 	[bits 16]
 	[org 0x7c00]			; BIOS Code load adress
 	mov bp, 0x8000 			; move base stack pointer over the BIOS sector
 	mov sp, bp  			; load base pointer to stack pointer
+;#############################################################################################
 	
 ;#############################################################################################
-; detect drives, show menu entrys accordingly used following registers for following medium:
+; detect drives, show menu entries accordingly used following registers for following medium:
 ; HDD:			CD:		Floppy(?)	
 ;
 ;	AH=01 --> int 13 -> gives status to al
@@ -46,22 +49,17 @@
 	mov ah, 0x13			; precursor to interrupt 13,01
 	int 13				; INT 13,1 - Get Status
 	cmp al, 0x00
-	je __NoError
-	call __ErrChk	
+	je __NoError	
 
 __ErrChk:
-	cmp al, [cl]			; try all possible combinations print error msg accordingly
+	cmp al, cl			; try all possible combinations print error msg accordingly
 	je __ErrFoundIn
-	
+	inc cl
 	cmp cl, 0xff			; end of error codes
 	je __Menu 
 	
 	jmp __ErrChk
 	
-
-
-
-
 __HelpFag:
 	and bx, 0x00FF	
 	add bx, 0x0037			; add 55 to hex, so it's legible as Hex to ASCII
@@ -78,44 +76,42 @@ __HelpFag2:
 	int 0x10
 	ret
 	
-	
-__ErrFoundIn:
-
-	mov ch, 0x02
-	
+__ErrFoundIn:	
+	mov ch, 0x02			; counter declared for __ErrFound
 	mov bx, ErrMsg
 	call __printString16
 
-_ErrFound	
+
+__ErrFound:	
 	mov bl, al
-	and bl, 0x0F			; get right char
 	mov bh, al
+	and bl, 0x0F			; get right char
 	and bh, 0xF0			; get left char
 	shr bh, 4			; shift 4 bit one hex
 	
-	mov es,bh			; save bh on es
-		
+	;mov es,bh			; save bh on es
+	mov dx,bx			; save bh on es
+	shr dx,8
+	and dx, 0x00FF
+
 	cmp bl, 0x0A			; if bigger than 9 add 55
 	jge __HelpFag
 	cmp bl, 0x09			; if smaller than 9 add 48
 	jle __HelpFag2
 	
 	and bx, 0x0000
-	mov es, bl
-		
+	;mov es, bl			; restore bl ?
+	mov bx,dx
+	
 	dec ch
 	cmp ch, 0x00
 	jne __ErrFound
 
-	
 __NoError:				
 	mov ah, 0x08			; precursor to interrupt 13,08
 	int 13				; INT 13,8 - Get Current Drive Parameters (XT & newer)
-	
-	
-__Menu:	
 
-	
+__Menu:	
 	mov bx, Border
 	call __printString16
 	mov bx, MenuEntry1
@@ -125,17 +121,21 @@ __Menu:
 	mov bx, MenuEntry3
 	call __printString16
 	mov bx, Border
-	call __printString16
-
+	call __printString16	
+	
 	jmp $				; Endless Loop
 
+	%include "__printString16.asm"
 
-%include "__printString16.asm"
+;#############################################################################################
+	times 510-($-$$) db 0		;pads the sector until the last two bytes in 512B
+	dw 0xaa55			;last two bytes (word/wyde) with magic number! 
+;#############################################################################################
 
 ;DATA
 ErrMsg: 	db "Error Reading Devices, Error Code:",0
 
-Border:		db "*************************************", 0x0a. 0x0d, 0
-MenuEntry1:	db "* 1) Load Cylinder from the CD	*", 0x0a. 0x0d, 0
-MenuEntry2:	db "* 2) Load Cylinder from HDD		*", 0x0a. 0x0d, 0
-MenuEntry3:	db "* 2) Load Cylinder from Floppy	*", 0x0a. 0x0d, 0
+Border:		db "*************************************", 0x0a, 0x0d, 0
+MenuEntry1:	db "* 1) Load Cylinder from the CD	*", 0x0a, 0x0d, 0
+MenuEntry2:	db "* 2) Load Cylinder from HDD		*", 0x0a, 0x0d, 0
+MenuEntry3:	db "* 2) Load Cylinder from Floppy	*", 0x0a, 0x0d, 0
