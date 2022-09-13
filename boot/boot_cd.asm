d8b53af61cb98e96c3b40da24b99c57f258b2936
TotanForMen <gvtreis@gmail.com>
2022-09-13 18:06:34 +0300

    file read function has been included (still can be improved but works just fine)

--- boot/boot_cd.asm
+++ boot/boot_cd.asm
@@ -57,7 +57,7 @@ retry_loop:
 	
 wait_loop:
 	in al,dx
-	test al,1<<7		;wait untily the drive is not bussy
+	test al,1<<7		;wait untily the drive is not busy
 	jnz wait_loop
 	
 	test al,1		;check for an error if it exists retry it again up to four times
@@ -95,105 +95,22 @@ dw 0xaa55
 
 	
 	mov di,IDENTIFY_PACKET_DEVICE_DATA+512
-	mov eax,16
-	mov ecx,1
-	call __IDE_ATAPI_READx86
-	
-	mov eax,[IDENTIFY_PACKET_DEVICE_DATA+512+156+2]
-	mov ecx,[IDENTIFY_PACKET_DEVICE_DATA+512+156+10]
-	mov di,IDENTIFY_PACKET_DEVICE_DATA+512
-	shr ecx,11
-	call __IDE_ATAPI_READx86
-	
-	mov bx,IDENTIFY_PACKET_DEVICE_DATA+512
-	movzx ax,[bx+0]
-	add bx,ax
-directory_search:
-	xor ch,ch
-	movzx ax,[bx+0]
-	add bx,ax
-	mov al,[bx+0]
-	test al,al
-	jz directory_search_not_found
-	mov al,[bx+32]	
-	cmp al,6
-	sete cl
-	add ch,cl
-	mov eax,[bx+33]
-	cmp eax,"KERN"
-	sete cl
-	add ch,cl
-	mov ax,[bx+33+4]
-	cmp ax,"EL"
-	sete cl
-	add ch,cl
-	mov ax,[bx+25]	
-	cmp ax,";1"
-	setne cl
-	add ch,cl
-	test ch,4
-	jz directory_search
-	
-	
-	mov eax,[bx+2]
-	mov ecx,[bx+10]
-	mov di,IDENTIFY_PACKET_DEVICE_DATA+512
-	shr ecx,11
-	call __IDE_ATAPI_READx86
-	mov bx,IDENTIFY_PACKET_DEVICE_DATA+512
-	movzx ax,[bx+0]
-	add bx,ax
-	
-	
-	directory_search2:
-	xor ch,ch
-	movzx ax,[bx+0]
-	add bx,ax
-	mov al,[bx+0]
-	test al,al
-	jz directory_search_not_found
-	mov al,[bx+32]	
-	cmp al,12
-	sete cl
-	add ch,cl
-	mov eax,[bx+33]
-	cmp eax,"KERN"
-	sete cl
-	add ch,cl
-	mov eax,[bx+33+4]
-	cmp eax,"EL.B"
-	sete cl
-	add ch,cl
-	mov eax,[bx+33+8]
-	cmp eax,"IN;1"
-	sete cl
-	add ch,cl
-	test ch,4
-	jz directory_search2
-	
-	
-	xor eax,eax
-	mov ecx,[bx+10]
-	mov di,IDENTIFY_PACKET_DEVICE_DATA+512
-	test cx,2047
-	setnz ah
-	shl ah,3
-	add ecx,eax
-	mov eax,[bx+2]
-	call __IDE_ATAPI_READx86
-	
+	mov si,kernel.dir
+	call __IDE_CD_FILE_READx86
+	call __binaryToDecimalx86
 	jmp IDENTIFY_PACKET_DEVICE_DATA+512
 	
 directory_search_not_found:
 	mov si,msg4
 	call __printStringx86
 	hlt
-	%include "../functions/__IDE_ATAPI_READx86.asm"
+	%include "../functions/__IDE_CD_FILE_READx86.asm"
 		
 msg1: db 10,"operating system found"
 enter: db 13,0
 msg2: db "IDE device not found",13,0
 msg3: db "IDE device found",13,0
 msg4: db "kernel.bin not found",0
+kernel.dir: db "/KERNEL/KERNEL.BIN;1/",0
 times 2048-($-$$) db 0
 IDENTIFY_PACKET_DEVICE_DATA:
