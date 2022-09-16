0976feeccfad81c11695503c6828f7fdecc5cb8b
TotanForMen <gvtreis@gmail.com>
2022-09-17 02:10:36 +0300

    a side_tool for priöitive image display has been added

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -4,13 +4,13 @@ kernel:
 get_eip:
 	pop edi
 	sub edi,get_eip-kernel
-	lea esi,[edi+msg1]
-	mov ah,0x0f
+	lea esi,[edi+coffee]
+	mov ah,0x70
 	call __printString_32
 	hlt
 msg1:	db	13,13,"Kernel has been loaded",13,0
-
 %include "../functions32/__printString_32.asm"
+%include "../side_tools/image.asm"
 times 510-($-$$) db 0
 dw 0xaa55
 
